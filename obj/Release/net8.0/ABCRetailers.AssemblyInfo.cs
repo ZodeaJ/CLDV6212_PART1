6d7ff235328c354ebdeee07be55c19c1bc4905e3
Zodea Jacobie <ST10440733@imconnect.edu.za>
2025-10-06 15:08:31 +0200

    First push

--- obj/Release/net8.0/ABCRetailers.AssemblyInfo.cs
+++ obj/Release/net8.0/ABCRetailers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d008d09d765d0630227d06c78041d9b98698f56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e368abd73115af0a6a0cea1ca20ca99487e6cd25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
